0513c5c3c6c7c7572f72637022977bd09c606592
Swaggypppp <121333979+Swaggypppp@users.noreply.github.com>
2022-12-24 15:50:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -24,28 +24,22 @@ ace4a078a947e787808a0054eeb3d36d',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oN_Rg574GXsClxjfCyQYdb6th5rA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'VzqB5LN-8mnItQAb1KbeVzQ5V6-TJifMWdZtgj1MO7A',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-31',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
-        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '*生日', name: '小居', year: '2003', date: '10-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2019', date: '04-01',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        { keyword: 'love_day', date: '2019-04-01' },
       ],
     },
   ],
@@ -58,7 +52,7 @@ ace4a078a947e787808a0054eeb3d36d',
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oN_Rg5wyG4T2cvYDpy-IMnFualZA',
     }
   ],
 
